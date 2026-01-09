b3be788e8c83c82d1d34f026d57e585582d2c84d
Mohamed Karam <mohamedkaramm500@gmail.com>
2026-01-09 17:13:00 +0200

    Write validation for sls_details table, and I add it in silver layer schame

--- scripts/silver/validation.sql
+++ scripts/silver/validation.sql
@@ -62,3 +62,43 @@ WHERE prd_end_dt < prd_start_dt
 
 
 SELECT * FROM silver.crm_prd_info
+
+
+-- ==================================== CRM SLS DETAILS ==================================== --
+
+
+-- Check for Invalid Dates
+SELECT
+NULLIF(sls_order_dt, 0) AS sls_order_dt
+FROM silver.crm_sales_details
+WHERE sls_order_dt <= 0 
+OR LEN(sls_order_dt) != 8
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
+
+-- Check for Invalid Dates Orders
+SELECT 
+*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+
+    -- Check Data Consistency: Between Sales, Qty, and Price
+    -- >> Sales = Qty * Price
+    -- Values must not be NULL, zer, or negtive 
+    SELECT DISTINCT
+    sls_quantity,
+    sls_sales,
+    sls_price
+
+    FROM silver.crm_sales_details
+    WHERE sls_sales != sls_quantity * sls_price
+    OR sls_sales IS NULL
+    OR sls_quantity IS NULL
+    OR sls_price IS NULL
+
+    OR sls_sales <= 0
+    OR sls_quantity <= 0
+    OR sls_price <= 0
+
+SELECT * FROM silver.crm_sales_details
